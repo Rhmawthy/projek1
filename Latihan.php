f70ab32849e04e4e8dc50f3aa27be6c52a62389c
Rhmawthy <rhmawthy.amma18024@gmail.com>
2023-03-12 10:29:33 +0800

    commit pertama

---
+++ Latihan.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>latihan</title>
+</head>
+<body>
+	<form action="prosesLatihan.php" method="post" name="input">
+		Jumlah semen :<input type="number" name="jumlah"><br>
+		<input type = "submit" nama = "Input" value = "Input">
+		
+	</form>
+
+</body>
+</html>
+
---
+++ prosesLatihan.php
@@ -0,0 +1,37 @@
+<?php
+
+	if (isset ($_POST['Input'])){
+		$jumlah = $_POST['jumlah'];
+		echo "jumlah semen : $jumlah <b>";
+
+		$hasil = $jumlah * 60000;
+		echo "total harga = $hasil <br>";
+
+	    if ($hasil >= 200){
+			echo"anda mendapatkan diskon 5%";
+		}
+	}
+
+
+
+		
+    
+
+    /*$hasil = $jumlah * 60000;
+
+    echo "total harga = $hasil <br>";
+    
+    if ($hasil >= 200){
+    	echo "anda mendapatkan diskon 5%";
+    }
+    else if ($hasil >= 100){
+    	echo "anda mendapatkan diskon 2%";
+    }
+    else{
+    	echo "tidak ada diskon";
+    }*/
+
+		
+	
+
+?>
